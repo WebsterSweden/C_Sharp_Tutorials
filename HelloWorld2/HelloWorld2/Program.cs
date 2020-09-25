4c536046e8a5a75c9a5ca394a2898ed1a6a7b178
WebsterSweden <webster@webster.se>
2020-09-25 21:17:42 +0200

    Edit HelloWorld2.

--- HelloWorld2/HelloWorld2/Program.cs
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace HelloWorld2
 {
@@ -7,6 +7,13 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nWhat is your name?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello {name}, on {date:d} at {date:t}");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
