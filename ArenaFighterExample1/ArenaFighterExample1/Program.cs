cd836a07f78422310f2a585430bba49e37a6d23e
WebsterSweden <webster@webster.se>
2020-10-01 23:12:49 +0200

    Create Battle.cs, Character.cs, Round.cs. Edit ArenaFighterExample1.

--- ArenaFighterExample1/ArenaFighterExample1/Program.cs
+++ ArenaFighterExample1/ArenaFighterExample1/Program.cs
@@ -4,11 +4,14 @@ namespace ArenaFighterExample1
 {
     class Program
     {
+        Random randomObjectTwo = new Random();
+
         static void Main(string[] args)
         {
             // Create an instance of the Random Object.
-            Random rnd = new Random(DateTime.Now.Millisecond);
-            Console.WriteLine(rnd);
+            Random randomObjectOne = new Random();
+            //Random randomObjectOne = new Random(DateTime.Now.Millisecond);
+            Console.WriteLine(randomObjectOne);
         }
     }
 }
---
+++ ArenaFighterExample1/ArenaFighterExample1/Round.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaFighterExample1
+{
+    class Round
+    {
+    }
+}
