b229904ee4b47ac8e47ba4038cb57f03cb9952c4
WebsterSweden <webster@webster.se>
2020-10-05 22:38:03 +0200

    Edit MikeDane1.

--- MikeDane1/MikeDane1/Program.cs
+++ MikeDane1/MikeDane1/Program.cs
@@ -6,6 +6,94 @@ namespace MikeDane1
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World");
+            //Console.ReadLine();
+
+            //Console.WriteLine("      /|");
+            //Console.WriteLine("     / |");
+            //Console.WriteLine("    /  |");
+            //Console.WriteLine("   /   |");
+            //Console.WriteLine("  /    |");
+            //Console.WriteLine(" /     |");
+            //Console.WriteLine("/______|");
+            //Console.ReadLine();
+
+            //string stringOne = "four";
+
+            //Console.WriteLine("Sentence number one");
+            //Console.WriteLine("Sentence number two");
+            //Console.WriteLine("Sentence number three");
+            //Console.WriteLine("Sentence number {0}", stringOne);
+            //Console.ReadLine();
+
+            //string stringTwo = "123";
+            //string stringThree = "456";
+            //int intOne = 123;
+            //int intTwo = 456;
+            //Console.WriteLine(stringTwo + stringThree);
+            //Console.WriteLine(intOne + intTwo);
+            //Console.WriteLine(stringTwo + intOne);
+
+            //char charOne = 'A';
+            //Console.WriteLine(charOne);
+
+            //double doubleOne = 3.3;
+            //Console.WriteLine(doubleOne);
+
+            //bool boolTrue = true;
+            //Console.WriteLine(boolTrue);
+
+            //Console.WriteLine("Constant");
+
+            //Console.WriteLine("New\nLine");
+
+            //Console.WriteLine("\"Double quotation mark\"");
+            //Console.WriteLine("\'Single quoation mark\'");
+
+            //string sentenceOne = "This is a sentence.";
+            //Console.WriteLine(sentenceOne.Length);
+            //Console.WriteLine(sentenceOne.ToUpper());
+            //Console.WriteLine(sentenceOne.ToLower());
+            //Console.WriteLine(sentenceOne.Contains("This"));
+            //Console.WriteLine(sentenceOne.Contains("this"));
+            //Console.WriteLine(sentenceOne[0]);
+            //Console.WriteLine(sentenceOne.IndexOf("sentence"));
+            //Console.WriteLine(sentenceOne.IndexOf('a'));
+            //Console.WriteLine(sentenceOne.IndexOf('b'));
+            //Console.WriteLine(sentenceOne.Substring(10, 8));
+
+            //Console.WriteLine(1 + 3);
+            //Console.WriteLine(1 - 3);
+            //Console.WriteLine(1 / 3);
+            //Console.WriteLine(1 * 3);
+
+            //Console.WriteLine(3 % 1);
+
+            //Console.WriteLine(2 + 3 * 4);
+
+            //Console.WriteLine(Math.Abs(-3));
+
+            //Console.WriteLine(Math.Pow(3, 2));
+
+            //Console.WriteLine(Math.Sqrt(9));
+
+            //Console.WriteLine(Math.Max(3, 9));
+
+            //Console.WriteLine(Math.Min(3, 9));
+
+            //Console.WriteLine(Math.Round(1.5));
+
+            //Console.Write("Console.Write");
+            //Console.WriteLine("Console.WriteLine");
+
+            //Console.WriteLine("Test Console.Read");
+            //int consoleReadOne = Console.Read();
+            //Console.WriteLine(consoleReadOne);
+
+            Console.WriteLine("Test Console.ReadLine");
+            string consoleReadLineOne = Console.ReadLine();
+            Console.WriteLine(consoleReadLineOne);
+
         }
     }
 }
