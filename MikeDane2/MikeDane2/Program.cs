7d25d4faa655bb4db510a740739162cfc660cf42
WebsterSweden <webster@webster.se>
2020-10-06 20:47:59 +0200

    Edit MikeDane2.

--- MikeDane2/MikeDane2/Program.cs
+++ MikeDane2/MikeDane2/Program.cs
@@ -6,6 +6,66 @@ namespace MikeDane2
     {
         static void Main(string[] args)
         {
+            //Console.Write("Enter an integer number: ");
+            //int numberOne = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter another integer number: ");
+            //int numberTwo = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(numberOne + numberTwo);
+
+            //Console.Write("Enter a decimal number: ");
+            //double doubleOne = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter another decimal number: ");
+            //double doubleTwo = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(doubleOne + doubleTwo);
+
+            //Console.WriteLine("Roses are red,");
+            //Console.WriteLine("Violets are blue,");
+            //Console.WriteLine("Sugar is sweet,");
+            //Console.WriteLine("And so are you.");
+
+            //string color;
+            //string pluralNoun;
+            //string personName;
+
+            //Console.Write("Enter a color: ");
+            //color = Console.ReadLine();
+            //Console.Write("Enter a plural noun: ");
+            //pluralNoun = Console.ReadLine();
+            //Console.Write("Enter a persons name: ");
+            //personName = Console.ReadLine();
+
+            //Console.WriteLine("Roses are {0},", color);
+            //Console.WriteLine("{0} are blue,", pluralNoun);
+            //Console.WriteLine("Sugar is sweet,");
+            //Console.WriteLine("And so are {0}.", personName);
+
+            //string[] stringArrayOne = { "ElementOne", "ElementTwo", "ElementThree" };
+            //Console.WriteLine(stringArrayOne[1]);
+
+            //MethodOne();
+
+            //MethodTwo("MethodTwo");
+
+            //int cubeNumber = MethodThree(3);
+            //Console.WriteLine(cubeNumber);
         }
+
+        //static void MethodOne()
+        //{
+        //    Console.WriteLine("MethodOne");
+        //}
+
+        //static void MethodTwo(string parameter)
+        //{
+        //    Console.WriteLine(parameter);
+        //}
+
+        // 2^3 -> 2 * 2 * 2
+        // Two cube.
+        //static int MethodThree(int number)
+        //{
+        //    int result = number * number * number;
+        //    return result;
+        //}
     }
 }
