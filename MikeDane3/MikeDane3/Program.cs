6942764d203a455cb4bae5048c2e24030a32a55c
WebsterSweden <webster@webster.se>
2020-10-11 18:15:07 +0200

    Edit MikeDane3.

--- MikeDane3/MikeDane3/Program.cs
+++ MikeDane3/MikeDane3/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic.CompilerServices;
 using System;
 using System.Linq;
 
@@ -67,41 +68,121 @@ namespace MikeDane3
 
             //Console.WriteLine(numberMaxTwo);
 
-            Console.Write("Enter a number: ");
-            double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter Operator: ");
-            string operatorChoice = Console.ReadLine();
+            //Console.Write("Enter Operator: ");
+            //string operatorChoice = Console.ReadLine();
 
-            Console.Write("Enter a number: ");
-            double number2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
 
-            double result;
+            //double result;
 
-            if (operatorChoice == "+")
+            //if (operatorChoice == "+")
+            //{
+            //    result = number1 + number2;
+            //    Console.WriteLine(result);
+            //}
+            //else if (operatorChoice == "-")
+            //{
+            //    result = number1 - number2;
+            //    Console.WriteLine(result);
+            //}
+            //else if (operatorChoice == "/")
+            //{
+            //    result = number1 / number2;
+            //    Console.WriteLine(result);
+            //}
+            //else if (operatorChoice == "*")
+            //{
+            //    result = number1 * number2;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Operator");
+            //}
+
+            //int dayOfWeekInteger = (int)DateTime.Now.DayOfWeek;
+            int dayOfWeekInteger = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+            Console.WriteLine(dayOfWeekInteger);
+
+            string dayOfWeek = "";
+
+            if (dayOfWeekInteger == 0)
             {
-                result = number1 + number2;
-                Console.WriteLine(result);
+                dayOfWeek = "Sunday";
             }
-            else if (operatorChoice == "-")
+            else if (dayOfWeekInteger == 1)
             {
-                result = number1 - number2;
-                Console.WriteLine(result);
+                dayOfWeek = "Monday";
             }
-            else if (operatorChoice == "/")
+            else if (dayOfWeekInteger == 2)
             {
-                result = number1 / number2;
-                Console.WriteLine(result);
+                dayOfWeek = "Tuesday";
             }
-            else if (operatorChoice == "*")
+            else if (dayOfWeekInteger == 3)
             {
-                result = number1 * number2;
-                Console.WriteLine(result);
+                dayOfWeek = "Wednesday";
+            }
+            else if (dayOfWeekInteger == 4)
+            {
+                dayOfWeek = "Thursday";
+            }
+            else if (dayOfWeekInteger == 5)
+            {
+                dayOfWeek = "Friday";
+            }
+            else if (dayOfWeekInteger == 6)
+            {
+                dayOfWeek = "Saturday";
             }
             else
             {
-                Console.WriteLine("Invalid Operator");
+                dayOfWeek = "Unknown";
             }
+
+            Console.WriteLine("dayOfWeekInteger: " + dayOfWeekInteger + " dayOfWeek: " + dayOfWeek);
+
+            static string GetDay(int dayNumber)
+            {
+                string dayName;
+
+                switch (dayNumber)
+                {
+                    case 0:
+                        dayName = "Sunday";
+                        break;
+                    case 1:
+                        dayName = "Monday";
+                        break;
+                    case 2:
+                        dayName = "Tuesday";
+                        break;
+                    case 3:
+                        dayName = "Wednesday";
+                        break;
+                    case 4:
+                        dayName = "Thursday";
+                        break;
+                    case 5:
+                        dayName = "Friday";
+                        break;
+                    case 6:
+                        dayName = "Saturday";
+                        break;
+                    default:
+                        dayName = "Invalid Day Number";
+                        break;
+                }
+
+                return dayName;
+            }
+
+            Console.WriteLine(GetDay((int)DateTime.Now.DayOfWeek));
+
         }
     }
 }
