4223c1ceac4964c3ca1c25fa960bc00a2700342b
WebsterSweden <webster@webster.se>
2020-10-12 00:09:22 +0200

    Edit MikeDane4.

--- MikeDane4/MikeDane4/Program.cs
+++ MikeDane4/MikeDane4/Program.cs
@@ -6,6 +6,20 @@ namespace MikeDane4
     {
         static void Main(string[] args)
         {
+            string secretWord = "abc";
+            string guessedWord = "";
+            int numberOfGuesses = 0;
+
+            while (secretWord != guessedWord)
+            {
+                Console.Write("Guess a word: ");
+                guessedWord = Console.ReadLine();
+                numberOfGuesses++;
+            }
+
+            Console.WriteLine("Congratulations! Your guess is: {0}", guessedWord);
+            Console.WriteLine("The secret word is: {0}", secretWord);
+            Console.WriteLine("The number of guesses: {0}", numberOfGuesses);
         }
     }
 }
