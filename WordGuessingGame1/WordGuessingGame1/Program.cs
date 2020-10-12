63d443c55fd352e74b0fada122db7219156f08cf
WebsterSweden <webster@webster.se>
2020-10-12 17:05:06 +0200

    WordGuessingGame1 Functional version.

--- WordGuessingGame1/WordGuessingGame1/Program.cs
+++ WordGuessingGame1/WordGuessingGame1/Program.cs
@@ -11,8 +11,10 @@ namespace WordGuessingGame1
             int guessCount = 0;
             int guessLimit;
             bool guessStatus = false;
+            //char playGame;
 
             Intro();
+            GameStart();
 
             void Intro()
             {
@@ -21,25 +23,38 @@ namespace WordGuessingGame1
                 secretWord = Console.ReadLine();
                 Console.Write("Number of tries: ");
                 guessLimit = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Type # to terminate the program");
             }
 
-            while (guessedWord != secretWord && guessStatus == false)
+            void GameStart()
             {
-                Console.Write("Guess the secret word: ");
-                guessedWord = Console.ReadLine();
-                guessCount++;
-                Console.WriteLine("You have guessed {0} out of {1}", guessCount, guessLimit);
-
-                if (guessedWord == secretWord)
+                while (guessedWord != secretWord && guessStatus == false)
                 {
-                    Console.WriteLine("Win!");
-                    guessStatus = true;
-                }
+                    Console.Write("Guess the secret word: ");
+                    guessedWord = Console.ReadLine();
+                    guessCount++;
 
-                if (guessCount == guessLimit)
-                {
-                    Console.WriteLine("Loose!");
-                    guessStatus = true;
+                    if (guessedWord == "#")
+                    {
+                        break;
+                    }
+
+                    if (guessedWord == secretWord)
+                    {
+                        Console.WriteLine("Congratulations! You guessed {0} and the word is correct!", guessedWord);
+                        guessStatus = true;
+                    }
+                    else if (guessedWord != secretWord)
+                    {
+                        Console.WriteLine("Sorry! You guessed: {0} and it is not the correct word", guessedWord);
+                        Console.WriteLine("You have guessed {0} out of {1}", guessCount, guessLimit);
+                    }
+
+                    if (guessedWord != secretWord && guessCount == guessLimit)
+                    {
+                        Console.WriteLine("You have used all tries. You loose the game!");
+                        guessStatus = true;
+                    }
                 }
             }
         }
