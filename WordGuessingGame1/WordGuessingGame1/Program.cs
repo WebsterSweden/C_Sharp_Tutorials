cd3e7c909774e2631a2f43663f02536a8eabd72e
WebsterSweden <webster@webster.se>
2020-10-12 10:59:37 +0200

    WordGuessingGame1 Procedural version.

--- WordGuessingGame1/WordGuessingGame1/Program.cs
+++ WordGuessingGame1/WordGuessingGame1/Program.cs
@@ -6,6 +6,42 @@ namespace WordGuessingGame1
     {
         static void Main(string[] args)
         {
+            string secretWord;
+            string guessedWord = "";
+            int guessCount = 0;
+            int guessLimit;
+            bool guessStatus = false;
+
+            Intro();
+
+            void Intro()
+            {
+                Console.WriteLine("Welcome to the Word Guessing Game!\nEnter a secret word and number of tries of guessing");
+                Console.Write("Secret word: ");
+                secretWord = Console.ReadLine();
+                Console.Write("Number of tries: ");
+                guessLimit = Convert.ToInt32(Console.ReadLine());
+            }
+
+            while (guessedWord != secretWord && guessStatus == false)
+            {
+                Console.Write("Guess the secret word: ");
+                guessedWord = Console.ReadLine();
+                guessCount++;
+                Console.WriteLine("You have guessed {0} out of {1}", guessCount, guessLimit);
+
+                if (guessedWord == secretWord)
+                {
+                    Console.WriteLine("Win!");
+                    guessStatus = true;
+                }
+
+                if (guessCount == guessLimit)
+                {
+                    Console.WriteLine("Loose!");
+                    guessStatus = true;
+                }
+            }
         }
     }
 }
