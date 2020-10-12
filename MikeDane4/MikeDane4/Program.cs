fbfe9e43c80fa13176e189f875e50f65319789c2
WebsterSweden <webster@webster.se>
2020-10-12 22:05:53 +0200

    Edit MikeDane4.

--- MikeDane4/MikeDane4/Program.cs
+++ MikeDane4/MikeDane4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.Serialization.Formatters;
 
 namespace MikeDane4
 {
@@ -6,35 +7,55 @@ namespace MikeDane4
     {
         static void Main(string[] args)
         {
-            string secretWord = "abc";
-            string guessedWord = "";
-            int guessCount = 0;
-            int guessLimit = 3;
-            bool outOffGuesses = false;
+            //string secretWord = "abc";
+            //string guessedWord = "";
+            //int guessCount = 0;
+            //int guessLimit = 3;
+            //bool outOffGuesses = false;
 
-            while (secretWord != guessedWord && !outOffGuesses)
+            //while (secretWord != guessedWord && !outOffGuesses)
+            //{
+            //    if (guessCount < guessLimit)
+            //    {
+            //        Console.Write("Guess a word: ");
+            //        guessedWord = Console.ReadLine();
+            //        guessCount++;
+            //    }
+            //    else
+            //    {
+            //        outOffGuesses = true;
+            //    }
+            //}
+
+            //if (outOffGuesses)
+            //{
+            //    Console.WriteLine("You Lose!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Congratulations! Your guess is: {0}", guessedWord);
+            //    Console.WriteLine("The secret word is: {0}", secretWord);
+            //    Console.WriteLine("The number of guesses: {0}", guessCount);
+            //}
+
+            int i = 1;
+            while (i <= 6)
             {
-                if (guessCount < guessLimit)
-                {
-                    Console.Write("Guess a word: ");
-                    guessedWord = Console.ReadLine();
-                    guessCount++;
-                }
-                else
-                {
-                    outOffGuesses = true;
-                }
+                Console.WriteLine(i);
+                i++;
             }
 
-            if (outOffGuesses)
+            for (i = 1; i <= 6; i++)
             {
-                Console.WriteLine("You Lose!");
+                Console.WriteLine(i);
             }
-            else
+
+            int[] numberArrayOne = { 1, 2, 3, 4, 5, 6 };
+
+            for (i = 0; i < numberArrayOne.Length; i++)
             {
-                Console.WriteLine("Congratulations! Your guess is: {0}", guessedWord);
-                Console.WriteLine("The secret word is: {0}", secretWord);
-                Console.WriteLine("The number of guesses: {0}", guessCount);
+                //Console.WriteLine("numberArrayOne[{0}]: ", i + " " + numberArrayOne[i]);
+                Console.WriteLine("numberArrayOne[" + i + "]: " + numberArrayOne[i]);
             }
         }
     }
