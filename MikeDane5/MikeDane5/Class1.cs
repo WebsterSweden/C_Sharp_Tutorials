a17e5cf8a0b211d28bed3d18f118002f805de3a8
WebsterSweden <webster@webster.se>
2020-10-13 21:35:57 +0200

    Edit MikeDane5.

---
+++ MikeDane5/MikeDane5/Class1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeDane5
+{
+    class Class1
+    {
+        public string class1String1 = "class1String1";
+        public string class1String2 = "class1String2";
+        public static int class1Int1 = 6;
+
+        public int Class1Method1()
+        {
+            string class1Method1String1 = "class1Method1String1";
+            string class1Method1String2 = "class1Method1String2";
+            int class1Method1Int1 = 3;
+            return class1Method1Int1;
+        }
+    }
+}
--- MikeDane5/MikeDane5/Program.cs
+++ MikeDane5/MikeDane5/Program.cs
@@ -6,6 +6,11 @@ namespace MikeDane5
     {
         static void Main(string[] args)
         {
+            Class1 class1Object = new Class1();
+            Console.WriteLine("class1Object.class1String1: " + class1Object.class1String1);
+            Console.WriteLine("class1Object.Class1Method1: " + class1Object.Class1Method1());
+
+            Console.WriteLine("Class1.class1Int1: " + Class1.class1Int1);
         }
     }
 }
