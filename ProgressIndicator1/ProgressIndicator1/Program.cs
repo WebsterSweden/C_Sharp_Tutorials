0fdcf99f96975b68d1d84e6d222deb02669475e1
WebsterSweden <webster@webster.se>
2020-11-03 13:06:10 +0100

    Edit ProgressIndicator1.

--- ProgressIndicator1/ProgressIndicator1/Program.cs
+++ ProgressIndicator1/ProgressIndicator1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace ProgressIndicator1
 {
@@ -6,6 +7,31 @@ namespace ProgressIndicator1
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                //Console.WriteLine(i);
+                //Console.Write(i);
+
+                //Console.WriteLine($"\n{i}");
+                //Console.Write($"\n{i}");
+
+                //Console.WriteLine($"\r{i}");
+                //Console.Write($"\r{i}");
+
+                //Console.WriteLine($"\t{i}");
+                //Console.Write($"\t{i}");
+
+                Console.Write($"\rProgress Indicator    ");
+                Thread.Sleep(500);
+                Console.Write($"\rProgress Indicator .");
+                Thread.Sleep(500);
+                Console.Write($"\rProgress Indicator ..");
+                Thread.Sleep(500);
+                Console.Write($"\rProgress Indicator ...");
+                Thread.Sleep(500);
+                Console.Write($"\rProgress Indicator ...");
+                Thread.Sleep(500);
+            }
         }
     }
 }
