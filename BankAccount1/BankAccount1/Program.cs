f6bcaae02f0e2581fcacc884c4d804b70d490c1d
WebsterSweden <webster@webster.se>
2020-11-17 14:52:09 +0100

    Edit BankAccount1.

--- BankAccount1/BankAccount1/Program.cs
+++ BankAccount1/BankAccount1/Program.cs
@@ -6,6 +6,35 @@ namespace BankAccount1
     {
         static void Main(string[] args)
         {
+            var account = new BankAccount("Firstname Lastname", 1200);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithDrawal(600, DateTime.Now, "Withdrawal One");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Deposit One");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -64);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            //try
+            //{
+            //    account.MakeWithDrawal(1200, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
         }
     }
 }
---
+++ BankAccount1/BankAccount1/Transaction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount1
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+
+        public DateTime Date { get; }
+
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
