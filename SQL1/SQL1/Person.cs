f8a483c78d735ece85a05636e2ac23f373fe8a44
WebsterSweden <webster@webster.se>
2020-12-21 14:28:48 +0100

    Edit SQL1.

---
+++ SQL1/SQL1/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQL1
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
--- SQL1/SQL1/Program.cs
+++ SQL1/SQL1/Program.cs
@@ -6,6 +6,35 @@ namespace SQL1
     {
         static void Main(string[] args)
         {
+            Person personOne = new Person
+            {
+                Id = 1,
+                Name = "FirstnameOne LastnameOne",
+                PhoneNumber = "123456",
+                Location = "Small City",
+            };
+
+            Person personTwo = new Person
+            {
+                Id = 2,
+                Name = "FirstnameTwo LastnameTwo",
+                PhoneNumber = "456789",
+                Location = "Medium City",
+            };
+
+            Person personThree = new Person
+            {
+                Id = 3,
+                Name = "FirstnameThree LastnameThree",
+                PhoneNumber = "789123",
+                Location = "Big City",
+            };
+
+            Console.WriteLine($"{personOne.Id} {personOne.Name} {personOne.PhoneNumber} {personOne.Location}");
+
+            Console.WriteLine($"{personTwo.Id} {personTwo.Name} {personTwo.PhoneNumber} {personTwo.Location}");
+
+            Console.WriteLine($"{personThree.Id} {personThree.Name} {personThree.PhoneNumber} {personThree.Location}");
         }
     }
 }
